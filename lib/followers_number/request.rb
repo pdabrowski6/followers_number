bb1aa10f83a553b023eceadacff33e31da6e410c
Paweł Dąbrowski <dziamber@gmail.com>
2019-04-21 13:36:14 +0200

    Upgrade request class to use headers

--- lib/followers_number/request.rb
+++ lib/followers_number/request.rb
@@ -1,7 +1,14 @@
 module FollowersNumber
   class Request
     def self.get(profile_url)
-      ::RestClient.get(profile_url)
+      headers = {
+        "Content-Language" => "en-US",
+        "Accept-Language" => "en-US",
+        "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36",
+        "Cookie" => ""
+      }
+      
+      ::RestClient::Request.execute(method: :get, url: profile_url, headers: headers)
     end
   end
 end
--- spec/lib/followers_number/request_spec.rb
+++ spec/lib/followers_number/request_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 
 RSpec.describe FollowersNumber::Request do
   describe '.get' do
+    let(:headers) do
+      {
+        "Content-Language" => "en-US",
+        "Accept-Language" => "en-US",
+        "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36",
+        "Cookie" => ""
+      }
+    end
+
     it 'returns request response' do
       response = double('response')
       url = 'https://domain.com'
-      allow(RestClient).to receive(:get).with(url).and_return(response)
+      allow(RestClient::Request).to receive(:execute)
+        .with(method: :get, url: url, headers: headers)
+        .and_return(response)
 
       expect(described_class.get(url)).to eq(response)
     end
